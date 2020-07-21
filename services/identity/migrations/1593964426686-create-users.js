9c99c11a1efede7584549420ad2a3749656782ff
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-07-21 21:30:00 +0200

    adjustted users migration

--- services/identity/migrations/1593964426686-create-users.js
+++ services/identity/migrations/1593964426686-create-users.js
@@ -3,16 +3,15 @@ const { createConnection } = require('../lib/db-util');
 module.exports.up = async next => {
   const db = await createConnection();
   await db.query(`CREATE TABLE users(
-    id varchar(64) unique primary key,
+    id char(36) unique primary key,
     username varchar(64) unique not null,
     password varchar(192) not null,
-    created_at date not null default now()
+    public_key text not null,
+    mfa_seed varchar(64)
   );`);
-  next();
 };
 
 module.exports.down = async next => {
   const db = await createConnection();
-  await db.query('DROP TABLE users TRUNCATE;');
-  next();
+  await db.query('DROP TABLE users CASCADE;');
 };
