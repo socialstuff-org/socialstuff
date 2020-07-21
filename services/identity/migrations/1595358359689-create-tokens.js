3e8fc95005c4c681d7739cc8775ba31c4bad1535
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-07-21 21:30:07 +0200

    added user tokens migration

---
+++ services/identity/migrations/1595358359689-create-tokens.js
@@ -0,0 +1,16 @@
+const { createConnection } = require('../lib/db-util');
+
+module.exports.up = async next => {
+  const db = await createConnection();
+  await db.query(`CREATE TABLE tokens(
+    token char(128) not null unique,
+    expires_at date not null,
+    id_user char(64) not null,
+    foreign key (id_user) references users(id)
+  );`);
+};
+
+module.exports.down = async next => {
+  const db = await createConnection();
+  await db.query('DROP TABLE tokens CASCADE;');
+};
