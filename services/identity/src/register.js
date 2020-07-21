987259d4897fdfd68249d114fde97a81d27cc060
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-07-21 21:31:02 +0200

    implemented most of user registration

--- services/identity/src/register.js
+++ services/identity/src/register.js
@@ -6,7 +6,16 @@ import argon from 'argon2';
 
 const middleware = [
   body('username').isString().isLength({ min: 2 }).withMessage('The username should be at least 2 characters long!'),
+  body('username').custom(async username => {
+    if (!username) return;
+    const db = await sharedConnection();
+    const [[{userExists}]] = await db.query('SELECT COUNT(*) as `userExists` FROM users WHERE username LIKE ?;', [username]);
+    if (userExists) {
+      return Promise.reject('Username is already taken!');
+    }
+  }),
   body('password').isString().matches(/[a-zA-Z0-9]{10,}/).withMessage('Please choose a suitable password!'),
+  body('public_key').isString().isLength({ min: 128 }),
   rejectOnValidationError,
 ];
 
@@ -17,11 +26,15 @@ const middleware = [
  */
 async function register(req, res) {
   const db = await sharedConnection();
-  const userExists = await db.query('SELECT COUNT(*) FROM users WHERE username LIKE ?;', [req.body.username]);
-  console.dir(userExists);
-
   const hash = await argon.hash(req.body.password);
-  res.send('foobar');
+  const base64hash = Buffer.from(hash, 'utf-8').toString('base64');
+  const sql = 'INSERT INTO users (id, username, password, public_key) VALUES (uuid(), ?, ?, ?)';
+  try {
+    await db.query(sql, [req.body.username, base64hash, req.body.public_key]);
+    res.status(201).json({ data: 'Registered successfully!' });
+  } catch (e) {
+    res.status(500).end();
+  }
 }
 
 export default [middleware, register];
