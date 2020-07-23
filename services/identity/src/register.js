f74e3c335113c10cb5d8e8a68001a54ae445c1cf
florian-leeser <florian.leeser@icloud.com>
2020-07-23 15:51:06 +0200

    added some more comments

--- services/identity/src/register.js
+++ services/identity/src/register.js
@@ -6,11 +6,14 @@ import {generateToken}            from './token-helper';
 import {hashHmac, hashUnique}     from './security-helper';
 
 const middleware = [
+
+  // Add maximum AND minimum numbers of characters for username and password? Thought about possible sql injections...
+
   body('username').isString().isLength({min: 2}).withMessage('The username should be at least 2 characters long!'),
   body('username').custom(async username => {
 
-    // Isnt this one unnecessary since we are already checking if it is a string and if so it returns?
-    
+    // Isnt this one unnecessary since we are already checking if it is a string and if not it returns?
+
     if (!username) {
       return;
     }
@@ -21,6 +24,9 @@ const middleware = [
     }
   }),
   body('password').isString().matches(/[a-zA-Z0-9]{10,}/).withMessage('Please choose a suitable password!'),
+
+  // Add a maximum length?
+
   body('public_key').isString().isLength({min: 128}),
   rejectOnValidationError
 ];
@@ -43,10 +49,10 @@ async function register(req, res) {
   // IF CORRECTED: delete comments and let us know what was done or add TODO for better solution
 
   const sql = `
-SET @user_id := uuid();
-INSERT INTO users (id, username, password, public_key) VALUES (@user_id, ?, ?, ?);
-INSERT INTO tokens (id_user, token, expires_at) VALUES (@user_id, ?, DATE_ADD(NOW(), INTERVAL 1 DAY));
-`;
+    SET @user_id := uuid();
+    INSERT INTO users (id, username, password, public_key) VALUES (@user_id, ?, ?, ?);
+    INSERT INTO tokens (id_user, token, expires_at) VALUES (@user_id, ?, DATE_ADD(NOW(), INTERVAL 1 DAY));
+  `;
   try {
     await db.query(sql, [req.body.username, passwordHash, req.body.public_key, tokenHash]);
     await db.commit();
