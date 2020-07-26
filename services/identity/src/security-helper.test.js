7febbd31128cf0b5a0625377573cd1e6f8bcf3e9
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-07-26 16:35:57 +0200

    added test for unique hash verification

--- services/identity/src/security-helper.test.js
+++ services/identity/src/security-helper.test.js
@@ -1,4 +1,4 @@
-import {hashUnique} from './security-helper';
+import {hashUnique, verifyHashUnique} from './security-helper';
 
 describe('security-helper', () => {
   describe('hashUnique', () => {
@@ -7,5 +7,13 @@ describe('security-helper', () => {
       const hash2 = await hashUnique('foobar');
       expect(hash1).not.toBe(hash2);
     });
-  })
+  });
+
+  describe('verifyHashUnique', () => {
+    test('properly verifies a unique hash', async () => {
+      const hash = await hashUnique('foobar');
+      const hashVerified = await verifyHashUnique(hash, 'foobar');
+      expect(hashVerified).toBe(true);
+    });
+  });
 });
