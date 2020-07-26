0760dcb35e833640e39e835222b908f04052abdb
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-07-26 17:16:33 +0200

    switched to base64 encoding function

--- services/identity/src/token-helper.js
+++ services/identity/src/token-helper.js
@@ -1,4 +1,4 @@
-import {randomBytes} from './security-helper';
+import {encodeStringToBase64, randomBytes} from './security-helper';
 
 /**
  *
@@ -7,5 +7,5 @@ import {randomBytes} from './security-helper';
 export async function generateToken(length = 64) {
   /**  @type {Buffer} */
   const bytes = await randomBytes(length);
-  return Buffer.from(bytes).toString('base64');
+  return encodeStringToBase64(bytes);
 }
