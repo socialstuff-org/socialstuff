aac877237d31790cafff7b5ca47ac610777b468d
Florian Leeser <florian.leeser@icloud.com>
2020-07-26 20:23:25 +0200

    Added token-helper test

---
+++ services/identity/src/token-helper.test.js
@@ -0,0 +1,16 @@
+import {decrypt, encrypt, hashUnique, passwordIssues, verifyHashUnique} from './security-helper';
+import { generateToken } from './token-helper';
+
+describe('token-helper', () => {
+  describe('generateToken', () => {
+    test('Generates a token with standard size of 64 characters', async () => {
+      const token = await generateToken();
+      expect(token.length == 64);
+    });
+
+    test('Generates a token with a given length', async () => {
+      const token = await generateToken(20);
+      expect(token.length== 20);
+    });
+  });
+});
