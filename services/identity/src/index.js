90a220f39d56e73579404e8b82b55ea0e2ede938
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-08-11 20:12:17 +0200

    updated index

--- services/identity/src/index.js
+++ services/identity/src/index.js
@@ -1,24 +1,27 @@
-import express            from 'express';
-import bodyParser         from 'body-parser';
-import register           from './register';
-import login              from './login';
-import customEnv          from 'custom-env';
-import util                                from 'util';
 import {rebuildDatabase, sharedConnection} from './db-util';
+import bodyParser                          from 'body-parser';
+import customEnv                           from 'custom-env';
+import express                             from 'express';
+import login                               from './login';
+import register                            from './register';
+import util                                from 'util';
 
-const ENV = process.env.NODE_ENV || 'dev';
+const ENV      = process.env.NODE_ENV || 'dev';
 const APP_PORT = parseInt(process.env.APP_PORT || '3000');
 const APP_HOST = process.env.APP_HOST || '0.0.0.0';
 
 customEnv.env(ENV);
 
 (async () => {
-  while (true) {
-    try {
-      await sharedConnection();
-      break;
-    } catch (e) {
-      console.error(e);
+  {
+    let retryConnection = true;
+    while (retryConnection) {
+      try {
+        await sharedConnection();
+        retryConnection = false;
+      } catch (e) {
+        console.error(e);
+      }
     }
   }
 
@@ -36,11 +39,12 @@ customEnv.env(ENV);
   app.post('/register', register);
   app.post('/login', login);
 
-  app.listen(APP_PORT, APP_HOST, err => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Social Stuff Identity service running on ${APP_HOST}:${APP_PORT}.`);
-  });
+  try {
+    await util.promisify(app.listen.bind(app))(APP_PORT, APP_HOST);
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`Social Stuff Identity service running on ${APP_HOST}:${APP_PORT}.`);
 })();
+
