e668db78b8f1065878afbc9adc1bf6d5b300a060
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-08-12 00:58:35 +0200

    fixed uuid errors

--- services/identity/src/register.js
+++ services/identity/src/register.js
@@ -1,9 +1,8 @@
-import {USERNAME_REGEX, hashHmac, hashUnique, passwordIssues} from './security-helper';
-import {body}                                                 from 'express-validator';
-import {generateToken}                                        from './token-helper';
-import {rejectOnValidationError}                              from './express-util';
-import {sharedConnection}                                     from './db-util';
-import { v1 as v1uuid } from 'uuid';
+import {USERNAME_REGEX, hashUnique, passwordIssues} from './security-helper';
+import {body}                                       from 'express-validator';
+import {rejectOnValidationError}                    from './express-util';
+import {sharedConnection}                           from './db-util';
+import { v1 as v1uuid }                             from 'uuid';
 
 const middleware = [
   body('username').isString().matches(USERNAME_REGEX).withMessage('Please pick a username containing regular characters (a-zA-Z), numbers, and \'_\' and \'.\', with a length between 5 and 20 characters!'),
@@ -41,16 +40,15 @@ const middleware = [
 async function register(req, res) {
   const db           = await sharedConnection();
   const passwordHash = await hashUnique(req.body.password);
-  const token        = await generateToken();
-  const tokenHash    = await hashHmac(Buffer.from(token, 'base64'));
   await db.beginTransaction();
 
   const id = v1uuid().replace(/-/g, '');
+  const token = v1uuid();
   const addUserSql = 'INSERT INTO users (id, username, password, public_key) VALUES (unhex(?), ?, ?, ?);';
-  const addLoginTokenSql = 'INSERT INTO tokens (id_user, token, expires_at) VALUES (unhex(?), ?, DATE_ADD(NOW(), INTERVAL 1 DAY));';
+  const addLoginTokenSql = 'INSERT INTO tokens (id_user, token, expires_at) VALUES (unhex(?), unhex(?), DATE_ADD(NOW(), INTERVAL 1 DAY));';
   try {
     await db.query(addUserSql, [id, req.body.username, passwordHash, req.body.public_key]);
-    await db.query(addLoginTokenSql, [id, tokenHash]);
+    await db.query(addLoginTokenSql, [id, token.replace(/-/g, '')]);
     await db.commit();
     res.status(201).json({data: {message: 'Registered successfully!', token}});
   } catch (e) {
