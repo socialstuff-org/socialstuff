08bd291a7af0a76bd76046c88ee4b2be98534cde
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-08-12 00:24:07 +0200

    disabled multiple statements

--- services/identity/src/db-util.js
+++ services/identity/src/db-util.js
@@ -7,11 +7,10 @@ import migrate from 'migrate';
  */
 export function createConnection() {
   return mysql.createConnection({
-    host:               process.env.MYSQL_HOST,
-    user:               process.env.MYSQL_USER,
-    password:           process.env.MYSQL_PASSWORD,
-    database:           process.env.MYSQL_DATABASE,
-    multipleStatements: true,
+    host:     process.env.MYSQL_HOST,
+    user:     process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
   });
 }
 
