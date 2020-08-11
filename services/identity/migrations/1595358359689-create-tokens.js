f1b853589f8b2d3540ac4bd817fc4f84f66171b9
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-08-12 00:59:37 +0200

    added 'if not exists' to table create statement and switched to binary token field

--- services/identity/migrations/1595358359689-create-tokens.js
+++ services/identity/migrations/1595358359689-create-tokens.js
@@ -2,8 +2,9 @@ const { createConnection } = require('../lib/db-util');
 
 module.exports.up = async next => {
   const db = await createConnection();
-  await db.query(`CREATE TABLE tokens(
-    token varchar(255) not null unique,
+  // TODO add index for `token` column
+  await db.query(`CREATE TABLE IF NOT EXISTS tokens(
+    token binary(16) not null unique,
     expires_at date default null,
     id_user BINARY(16) not null,
     foreign key (id_user) references users(id)
