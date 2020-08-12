dd705a05639aca5ed007698a79edb746239e8b92
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-08-12 23:16:16 +0200

    added 'secret' field

--- services/identity/migrations/1597186025511-create-registration-confirmation.js
+++ services/identity/migrations/1597186025511-create-registration-confirmation.js
@@ -3,8 +3,8 @@ const { createConnection } = require('../lib/db-util');
 module.exports.up = async next => {
   const db = await createConnection();
   await db.query(`CREATE TABLE IF NOT EXISTS registration_confirmations(
-    token binary(16) not null unique,
     expires_at date not null,
+    secret varchar(50) not null,
     id_user BINARY(16) not null,
     foreign key (id_user) references users(id)
   );`);
