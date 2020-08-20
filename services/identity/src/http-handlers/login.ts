5e74f2056a9cbbcec768b28cd02d4c555e89f004
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-08-20 22:00:26 +0200

    fixed bugs and added license hint

--- services/identity/src/http-handlers/login.ts
+++ services/identity/src/http-handlers/login.ts
@@ -1,8 +1,24 @@
+// This file is part of SocialStuff Identity.
+//
+// SocialStuff Identity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SocialStuff Identity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with SocialStuff Identity.  If not, see <https://www.gnu.org/licenses/>.
+
 import {Request, Response}          from 'express';
 import {body}                       from 'express-validator';
 import {sharedConnection}           from '../utilities/mysql';
 import {rejectOnValidationError}    from '../utilities/express';
 import {hashHmac, verifyHashUnique} from '../utilities/security';
+import {RowDataPacket}              from 'mysql2/promise';
 
 const middleware = [
   body('username').isString().isLength({min: 5, max: 20}).withMessage('This is not a valid username.'),
@@ -15,7 +31,7 @@ async function login(req: Request, res: Response) {
 
   const sql = 'SELECT id,password as passwordHash FROM users WHERE username LIKE ?;';
 
-  const [userRow] = await db.query(sql, [req.body.username]);
+  const [userRow] = await db.query(sql, [req.body.username]) as RowDataPacket[][];
 
   if (!userRow.length) {
     res.status(400).json({errors: [{message: 'Invalid credentials!'}]}).end();
@@ -31,7 +47,7 @@ async function login(req: Request, res: Response) {
 
   const token = '';
   // TODO generate uuid as token
-  const addTokenSql = 'INSERT INTO tokens (value, id_user, expires_at) VALUES (?,?,DATE_ADD(NOW(), INTERVAL 1 DAY));';
+  const addTokenSql = 'INSERT INTO tokens (token, id_user, expires_at) VALUES (?,?,DATE_ADD(NOW(), INTERVAL 1 DAY));';
   try {
     await db.query(addTokenSql, [hashHmac(token), id]);
   } catch (e) {
