8f75002189e82fb019df43454ef685e4ff7a67e0
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-08-20 20:26:12 +0200

    extended bootstrapping; bootstrapping now returns itself

--- services/identity/src/bootstrap.ts
+++ services/identity/src/bootstrap.ts
@@ -1,10 +1,12 @@
-import customEnv                           from 'custom-env';
-import {createConnection, rebuildDatabase} from './utilities/mysql';
+// @ts-ignore
+import customEnv                                             from 'custom-env';
+import {createConnection, rebuildDatabase, sharedConnection} from './utilities/mysql';
+import {v1}                                                  from 'uuid';
 
 const ENV = process.env.NODE_ENV || 'dev';
 customEnv.env(ENV);
 
-(async () => {
+export default (async () => {
   {
     let retryConnection = true;
     while (retryConnection) {
@@ -12,7 +14,7 @@ customEnv.env(ENV);
         await createConnection();
         retryConnection = false;
       } catch (e) {
-        console.error('retrying initial connection...');
+        console.error('retrying initial connection...', e);
         await new Promise(res => {
           setTimeout(res, 1000);
         });
@@ -20,9 +22,17 @@ customEnv.env(ENV);
     }
   }
 
-  if (ENV === 'dev') {
-    console.log('Setting up database...');
-    await rebuildDatabase();
-    console.log('Database ready for use!');
+  const db = await sharedConnection();
+
+  if (ENV !== 'dev') {
+    return;
   }
+
+  console.log('Setting up database...');
+  await rebuildDatabase();
+  console.log('Database ready for use!');
+  console.log('seeding some data...');
+  const id = v1().replace(/-/g, '');
+  await db.query('INSERT INTO registration_invites (secret, expires_at) VALUES (?, DATE_ADD(NOW(), INTERVAL 1 DAY));', [id]);
+  console.log('sample invite code:', id);
 })();
