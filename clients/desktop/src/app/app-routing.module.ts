52d4f5c459818698b9affab7658d5c04c22836f7
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-08-24 22:05:43 +0200

    added electron project for desktop clients

---
+++ clients/desktop/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent
+  // }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
