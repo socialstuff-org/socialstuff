17f1e5f5b9865e26d68017738582ab3d579114f8
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-09-14 17:17:15 +0200

    added pproxy example to test container connection

--- services/chat/src/http-handlers/foo.ts
+++ services/chat/src/http-handlers/foo.ts
@@ -13,18 +13,34 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with SocialStuff Chat.  If not, see <https://www.gnu.org/licenses/>.
 
+import axios                                from 'axios';
 import {ValidationChain}                    from 'express-validator';
-import {Request, Response}                  from 'express';
+import {Response}                           from 'express';
 import {injectMongodbConnectionIntoRequest} from 'utilities/express';
 import {RequestWithDependencies}            from 'types/request-with-dependencies';
 
 export const middleware: ValidationChain[] = [
-  injectMongodbConnectionIntoRequest as any as ValidationChain
+  injectMongodbConnectionIntoRequest as any
 ];
 
+const defaultResponse = {
+  errors: {
+    identity: {
+      msg: 'Could not communicate with identity service!'
+    }
+  }
+};
+
 export async function foo(req: RequestWithDependencies, res: Response) {
-  const db = req.mongo!;
-  res.end('foo');
+  const target = req.env!.SOCIALSTUFF_IDENTITY_ENDPOINT + '/register';
+  console.log('target:', target);
+  try {
+    const identityResponse = await axios.post(target);
+    res.json(identityResponse.data).end();
+  } catch (e) {
+    console.error(Object.keys(e));
+    res.status(e.response.status || 500).json(e.response.data || defaultResponse).end();
+  }
 }
 
 export default [middleware, foo as any];
