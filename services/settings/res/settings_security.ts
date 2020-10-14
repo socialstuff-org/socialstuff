115e9c442d27b5aa86dac1f86ab7b3376ccf4a06
tobias-jansen-2411 <B3ll3n!95>
2020-10-14 11:00:17 +0200

    refactored format of settings_security.json document added getSettings and setSecSettings handler in settings_security_handler.ts

--- services/settings/res/settings_security.ts
+++ services/settings/res/settings_security.ts
@@ -1,15 +1,21 @@
 export const secSettings =
 {
-  "two_factor_auth": true,
-  "phone": true,
-  "email": false,
+  "two_factor_auth": {
+    "on" : true,
+    "phone": true,
+    "email": false
+  },
   "confirmed_emails_only": true,
-  "individual_pwd_req": true,
-  "upper-case": true,
-  "number": true,
-  "special_char": true,
-  "reg_ex": false,
-  "reg_ex_string": "",
-  "Inv_only": true,
-  "inv_only_by_adm": true
+  "individual_pwd_req": {
+    "on": true,
+    "upper_case": true,
+    "number": true,
+    "special_char": true,
+    "reg_ex": false,
+    "reg_ex_string": "",
+  },
+  "inv_only": {
+    "on": true,
+    "inv_only_by_adm": true
+  }
 }
--- services/settings/src/http-handlers/settings_security_handler.ts
+++ services/settings/src/http-handlers/settings_security_handler.ts
@@ -1,14 +1,33 @@
 import {secSettings} from "../../res/settings_security";
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 function getSecSettings() {
     return secSettings;
 }
+function changeSecuritySettings(req: Request, res: Response) {
 
-function setTwoFactorAuth(two_factor_auth: boolean, email: boolean, phone: boolean) {
-    secSettings.two_factor_auth = two_factor_auth;
-    secSettings.email = email;
-    secSettings.phone = phone;
+    var body = req.body;
+
+    secSettings.two_factor_auth = body.two_factor_auth
+
+    secSettings.two_factor_auth.on = body.two_factor_auth.on;
+    secSettings.two_factor_auth.phone = body.two_factor_auth.phone;
+    secSettings.two_factor_auth.email = body.two_factor_auth.email;
+
+    secSettings.confirmed_emails_only = body.confirmed_emails_only;
+
+    secSettings.individual_pwd_req.on = body.individual_pwd_req.individual_pwd_req;
+    secSettings.individual_pwd_req.number = body.individual_pwd_req.number;
+    secSettings.individual_pwd_req.special_char = body.individual_pwd_req.special_char;
+    secSettings.individual_pwd_req.upper_case = body.individual_pwd_req.upper_case;
+    secSettings.individual_pwd_req.reg_ex = body.individual_pwd_req.reg_ex;
+    secSettings.individual_pwd_req.reg_ex_string = body.individual_pwd_req.reg_ex_string;
+
+    secSettings.inv_only.on = body.inv_only.on;
+    secSettings.inv_only.inv_only_by_adm = body.inv_only_by_adm;
+
+    //TODO change response
+    return res.send({"status": 200});
 }
 
 var settingsInterface = express();
@@ -17,4 +36,9 @@ settingsInterface.get("/security-settings", (req, res) => {
 
     }
     res.send(getSecSettings())
-})
+});
+
+settingsInterface.post("/security-settings", ((req, res) => {
+
+    changeSecuritySettings(req, res);
+}));
