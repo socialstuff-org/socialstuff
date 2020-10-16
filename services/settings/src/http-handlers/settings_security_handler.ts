25b65bc06d1ca5167f475d3a694735f783757983
tobias-jansen-2411 <B3ll3n!95>
2020-10-16 15:11:54 +0200

    added express-validation instead of big if/else check

--- services/settings/src/http-handlers/settings_security_handler.ts
+++ services/settings/src/http-handlers/settings_security_handler.ts
@@ -2,6 +2,7 @@ import {Request, Response, Router} from 'express';
 import secSettings from '../res/security_settings.json'
 import instantiate = WebAssembly.instantiate;
 import {ErrorResponse} from '@socialstuff/utilities/responses'
+import {body, check, validationResult} from 'express-validator';
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
@@ -22,42 +23,18 @@ const editJsonFile = require("edit-json-file");
 let file = editJsonFile(__dirname + '/../res/security_settings.json');
 
 function changeSecuritySettings(req: Request, res: Response) {
-  const body = req.body;// bodyParser.urlencoded(req.body);// req.body;
-  //console.log("Body is empty: ", body === null);
-  //console.log(JSON.stringify(body));
-  //console.log("Two factor auth on is boolean: ", body.two_factor_auth.on.type)
-  console.log(file)
-  console.log(__dirname + '/../res/security_settings.json');
-  //console.log("False instance of Boolean: ", false instanceof boolean)
-  console.log("Boolean test", typeof body.two_factor_auth.on === "boolean")
-  console.log("String test",  typeof body.two_factor_auth.on === "string")
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("Errors in the Json occurred!")
+    return res.status(422).json({ errors: errors.array() })
+  }
 
-  console.log("tfOn", typeof body.two_factor_auth.on === "boolean");
-  console.log("tfPhone", typeof body.two_factor_auth.phone === "boolean");
-  console.log("tfEmail", typeof body.two_factor_auth.email === "boolean");
-  console.log("tfCEO", typeof body.confirmed_emails_only === "boolean");
-  console.log("pwreq_on", typeof body.individual_pwd_req.on === "boolean");
-  console.log("pw_req_nb", typeof body.individual_pwd_req.number === "boolean");
-  console.log("pw_req_sc", typeof body.individual_pwd_req.special_char === "boolean");
-  console.log("pw_req_uc", typeof body.individual_pwd_req.upper_case === "boolean");
-  console.log("pw_req_regEx", typeof body.individual_pwd_req.reg_ex === "boolean");
-  console.log("pw_req_regExStr", typeof body.individual_pwd_req.reg_ex_string === "string");
-  console.log("Inv_only", typeof body.inv_only.on === "boolean");
-  console.log("inv_only_admin", typeof body.inv_only.inv_only_by_adm === "boolean");
+
+  const body = req.body;// bodyParser.urlencoded(req.body);// req.body;
+
   //body.individual_pwd_req.reg_ex_string instanceof String
   //TODO express validator
-  if (((typeof body.two_factor_auth.on) === "boolean")
-    && ((typeof body.two_factor_auth.phone) === "boolean")
-    && ((typeof body.two_factor_auth.email) === "boolean")
-    && ((typeof body.confirmed_emails_only) === "boolean")
-    && ((typeof body.individual_pwd_req.on) === "boolean")
-    && ((typeof body.individual_pwd_req.number) === "boolean")
-    && ((typeof body.individual_pwd_req.special_char) === "boolean")
-    && ((typeof body.individual_pwd_req.upper_case) === "boolean")
-    && ((typeof body.individual_pwd_req.reg_ex) === "boolean")
-    && ((typeof body.individual_pwd_req.reg_ex_string) === "string")
-    && ((typeof body.inv_only.on) === "boolean")
-    && ((typeof body.inv_only.inv_only_by_adm) === "boolean")) {
+
 
 
     //secSettings.two_factor_auth = body.two_factor_auth;
@@ -77,18 +54,26 @@ function changeSecuritySettings(req: Request, res: Response) {
     file.save();
 
     res.json({data: secSettings});
-  } else {
-    //const error:ErrorResponse = {};
-    //res.status(500).json(error);
-    //res.status(500)
-
-  res.json({error: 500})
-  }
 }
+
+let validationParameters = [
+  check("two_factor_auth.on").isBoolean(),
+  check('two_factor_auth.phone').isBoolean(),
+  check('two_factor_auth.email').isBoolean(),
+  check('confirmed_emails_only').isBoolean(),
+  check('individual_pwd_req.on').isBoolean(),
+  check('individual_pwd_req.number').isBoolean(),
+  check('individual_pwd_req.special_char').isBoolean(),
+  check('individual_pwd_req.upper_case').isBoolean(),
+  check('individual_pwd_req.reg_ex').isBoolean(),
+  check('individual_pwd_req.reg_ex_string').isString(), //TODO find regExMatcher
+  check('inv_only.on').isBoolean(),
+  check('inv_only.inv_only_by_adm').isBoolean()];
+
 
 const settingsInterface = Router();
 settingsInterface.get('/', getSecSettings);
-settingsInterface.put('/', changeSecuritySettings);
+settingsInterface.put('/', validationParameters, changeSecuritySettings);
 
 export default settingsInterface;
 
