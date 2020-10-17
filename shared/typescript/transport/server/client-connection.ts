f30726a2763c7af8eb58ea5091ae876e0b7cf3c4
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-10-17 17:54:01 +0200

    massively reduced code duplication

--- shared/typescript/transport/server/client-connection.ts
+++ shared/typescript/transport/server/client-connection.ts
@@ -13,34 +13,15 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with TITP.  If not, see <https://www.gnu.org/licenses/>.
 
-import {Socket}                                       from 'net';
-import {BinaryLike, createCipheriv, createDecipheriv} from 'crypto';
-import {makeWriteP}                     from '../socket';
-import {fromEvent, Observable, Subject} from 'rxjs';
+import {Socket}                from 'net';
+import {CommonTitpClient}      from '../client/common';
+import {fromEvent, Observable} from 'rxjs';
 
 
-export class TitpClientConnection {
-  private _socket: Socket;
-  private readonly _username: string;
-  private readonly _write: (data: (Uint8Array | string)) => Promise<void>;
-  private readonly _key: Buffer;
-  private readonly _data: Subject<Buffer> = new Subject<Buffer>();
-  private _dataBuffer = Buffer.alloc(0);
-
-  constructor(socket: Socket, username: string, key: Buffer) {
-    this._socket = socket;
-    this._username = username;
-    this._write = makeWriteP(socket);
-    this._key = key;
-    this._socket.on('data', data => {
-      this._dataBuffer = Buffer.concat([this._dataBuffer, data]);
-      if (socket.writableLength) {
-        return;
-      }
-      const decipher = createDecipheriv('aes-256-cbc', this._key.slice(0, 32), this._key.slice(32));
-      const result = Buffer.concat([decipher.update(this._dataBuffer), decipher.final()]);
-      this._data.next(result);
-    });
+export class TitpClientConnection extends CommonTitpClient {
+  constructor(socket: Socket, username: string, protected _key: Buffer) {
+    super(username, socket);
+    this._init();
   }
 
   public on(event: 'close'): Observable<boolean>;
@@ -50,23 +31,5 @@ export class TitpClientConnection {
         return fromEvent(this._socket, 'close');
     }
     throw new Error(`Unknown event '${event}'!`);
-  }
-
-  public data(): Observable<Buffer> {
-    return this._data;
-  }
-
-  public username() {
-    return this._username;
-  }
-
-  public key() {
-    return this._key;
-  }
-
-  public write(data: BinaryLike) {
-    const cipher = createCipheriv('aes-256-cbc', this._key.slice(0, 32), this._key.slice(32));
-    const enc = Buffer.concat([cipher.update(data), cipher.final()]);
-    return this._write(enc);
   }
 }
