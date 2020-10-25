49b7613b6dd0ed3e8a3d2f02c93d077dad0cb551
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-10-25 19:59:01 +0100

    started to utilize new serialization functionality

--- shared/typescript/transport/message/index.ts
+++ shared/typescript/transport/message/index.ts
@@ -13,8 +13,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with TITP.  If not, see <https://www.gnu.org/licenses/>.
 
-import {KeyObject, randomBytes}                               from 'crypto';
-import {decryptAes384, decryptRsa, encryptAes384, encryptRsa} from '../crypto';
+import {KeyObject, randomBytes}                                              from 'crypto';
+import {decryptAes384, decryptRsa, encryptAes384, encryptRsa}                from '../crypto';
+import {deserialize, objectToDeserializationSchema, serialize, uIntToBuffer} from '../serial';
 
 export enum MessageContentType {
   textMessage,
@@ -28,8 +29,59 @@ export interface MessageAttachment {
   content: Buffer;
 }
 
+export const MessageDeserializationSchema = {
+  'sentAt':      {
+    'type': 'Date',
+  },
+  'type':        {
+    'type': 'number',
+  },
+  'content':     {
+    'type': 'Buffer',
+  },
+  'attachments': {
+    'type':    {
+      'content': {
+        'type': 'Buffer',
+      },
+      'name':    {
+        'type': 'string',
+      },
+    },
+    'isArray': true,
+  },
+};
+
+export const MessageEnvelopDeserializationSchema = {
+  'recipients':        {
+    'type':    'string',
+    'isArray': true,
+  },
+  'foreignRecipients': {
+    'type':    {
+      'address':    {
+        'type': 'string',
+      },
+      'recipients': {
+        'type': 'Buffer',
+      },
+    },
+    'isArray': true,
+  },
+  'content':           {
+    'type': 'Buffer',
+  },
+};
+
+interface SerializedMessageContent {
+  sentAt: Date;
+  type: MessageContentType;
+  content: Buffer;
+  attachments: MessageAttachment[];
+}
+
 export class MessageEnvelop {
-  constructor(private _recipients: string[], private _content: Buffer) {
+  constructor(private _content: Buffer, private _recipients: string[], private _foreignRecipients: { address: string, recipients: Buffer }[] = []) {
   }
 
   public content() {
@@ -40,7 +92,12 @@ export class MessageEnvelop {
     return this._recipients;
   }
 
+  public foreignRecipients() {
+    return this._foreignRecipients;
+  }
+
   public open(recipientRsaPrivateKey: KeyObject, conversationKeys: { [username: string]: Buffer }): Message {
+
     let content = this._content.slice();
     const senderNameLength = content.readUInt16BE();
     content = content.slice(2);
@@ -70,34 +127,25 @@ export class MessageEnvelop {
       content = content.slice(2);
       const name = content.slice(0, nameLength).toString('utf8');
       content = content.slice(nameLength);
-      attachments.push({ name, content: attachmentContent });
+      attachments.push({name, content: attachmentContent});
     }
     return new Message(type, senderName, sentAt, [], messageContent, attachments);
   }
 
   public serialize(): Buffer {
-    const parts: Buffer[] = [];
-    for (const recipient of this._recipients.map(x => Buffer.from(x, 'utf-8'))) {
-      const length = Buffer.alloc(1);
-      length.writeUInt8(recipient.length);
-      parts.push(length, recipient);
-    }
-    const divider = Buffer.alloc(2);
-    divider.writeUInt16BE(0);
-    parts.push(divider, this._content);
-    return Buffer.concat(parts);
+    const serializedEnvelop = {
+      recipients:        this._recipients,
+      foreignRecipients: this._foreignRecipients,
+      content:           this._content,
+    };
+    console.log('schema', JSON.stringify(objectToDeserializationSchema(serializedEnvelop), undefined, 2));
+    return serialize(serializedEnvelop);
   }
 
   public static deserialize(message: Buffer): MessageEnvelop {
-    const recipients: string[] = [];
-    let i = 0;
-    do {
-      const length = message.readUInt8(i);
-      const name = message.slice(i + 1, i + 1 + length).toString('utf8');
-      recipients.push(name);
-      i += length + 1;
-    } while(message.readUInt16BE(i) !== 0);
-    return new MessageEnvelop(recipients, message.slice(i + 2));
+    const {recipients, foreignRecipients, content} = deserialize(MessageEnvelopDeserializationSchema as any, message);
+    // recipients
+    return new MessageEnvelop(content, recipients, foreignRecipients);
   }
 }
 
@@ -136,32 +184,67 @@ export class Message {
     return this._sentAt;
   }
 
+  /**
+   * Encrypts the message and prepares it for the transmission to the server.
+   * @param key 384 bit aes key.
+   * @param rsaPublicKeyRecipient
+   * @param iv
+   */
   public seal(key: Buffer, rsaPublicKeyRecipient: KeyObject, iv: Buffer = randomBytes(32)): MessageEnvelop {
-    const bufferParts: Buffer[] = [
-      Buffer.from(this._sentAt.toISOString()),
-      Buffer.from([this._type])
-    ];
-    {
-      const contentLength = Buffer.alloc(4, 0);
-      contentLength.writeUInt32BE(this._content.length);
-      bufferParts.push(contentLength, this._content);
-    }
+    const s: SerializedMessageContent = {
+      sentAt:      this._sentAt,
+      type:        this._type,
+      content:     this._content,
+      attachments: this._attachments,
+    };
 
-    for (const attachment of this._attachments) {
-      const fileSize = Buffer.alloc(4, 0);
-      fileSize.writeUInt32BE(attachment.content.length);
-      const name = Buffer.from(attachment.name, 'utf8');
-      const nameLength = Buffer.alloc(2, 0);
-      nameLength.writeUInt16BE(name.length);
-      bufferParts.push(fileSize, attachment.content, nameLength, name);
-    }
-    bufferParts.push(Buffer.alloc(4, 0));
-    const serializedMessage = Buffer.concat(bufferParts);
+    const serializedMessage = serialize(s as any);
     const sealedMessage = encryptAes384(serializedMessage, key, {iv});
     const asyncSenderName = encryptRsa(this._sender, rsaPublicKeyRecipient);
-    const senderNameLength = Buffer.alloc(2);
-    senderNameLength.writeUInt16BE(asyncSenderName.length);
+    const senderNameLength = uIntToBuffer(asyncSenderName.length);
 
-    return new MessageEnvelop(this._recipients, Buffer.concat([senderNameLength, asyncSenderName, sealedMessage]));
+    const serverDistribution: { address: string, recipients: Buffer }[] = [];
+    const [senderUsername, senderServerStr] = this._sender.split('@');
+    if (!senderServerStr) {
+      throw new Error(`Missing server address in sender name '${this._sender}'!`);
+    }
+    const [senderHostname, senderPort] = [...senderServerStr.split(':'), '8086'];
+    const senderEndpoint = senderHostname + ':' + senderPort;
+    const sender = senderUsername + '@' + senderEndpoint;
+
+    const foreignReceivers: { address: string, recipients: Buffer }[] = [];
+
+    const foo: { [key: string]: string[] } = {};
+    {
+      for (const r of this._recipients) {
+        const [username, server] = [...r.toLowerCase().split('@'), 'default-server.com'];
+        const [hostname, portStr] = [...server.split(':'), '8086'];
+        const port = parseInt(portStr);
+        const endpoint = hostname + ':' + port;
+        if (!foo[endpoint]) {
+          foo[endpoint] = [];
+        }
+        foo[endpoint].push(username);
+      }
+
+      for (const name in foo) {
+        if (name === senderEndpoint) {
+          continue;
+        }
+        const recipientBuffers = foo[name].map(x => Buffer.from(x, 'utf-8'));
+        const recipients = [];
+        for (const b of recipientBuffers) {
+          const length = uIntToBuffer(b.length);
+          recipients.push(length, b);
+        }
+        foreignReceivers.push({address: name, recipients: Buffer.concat(recipients)});
+      }
+    }
+    const recipients = foo[senderEndpoint] || [];
+    return new MessageEnvelop(
+      Buffer.concat([senderNameLength, asyncSenderName, sealedMessage]),
+      recipients,
+      foreignReceivers,
+    );
   }
 }
