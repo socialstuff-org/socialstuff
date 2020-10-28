37a9709ac80fa2aa9d5edcab5a6e368a8002dd73
tobias-jansen-2411 <B3ll3n!95>
2020-10-28 10:57:45 +0100

    added cors parameter

--- services/settings/src/index.ts
+++ services/settings/src/index.ts
@@ -8,12 +8,13 @@ import router from './router';
 const APP_PORT = parseInt(process.env.APP_PORT || '3002');
 const APP_HOST = process.env.APP_HOST || '0.0.0.0';
 
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded());
-
+app.use(cors());
+app.options('*', cors())
 app.use('/', router);
 
 app.listen(APP_PORT, APP_HOST, () => {
