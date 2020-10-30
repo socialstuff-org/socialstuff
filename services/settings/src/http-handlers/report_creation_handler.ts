832a780b6a6d3f03001bbdddcdf92807537431e4
tobias-jansen-2411 <B3ll3n!95>
2020-10-30 18:52:26 +0100

    removed unnecessary postgres_client.ts

--- services/settings/src/http-handlers/report_creation_handler.ts
+++ services/settings/src/http-handlers/report_creation_handler.ts
@@ -2,7 +2,11 @@ import {Request, Response, Router} from 'express'
 import {getReportReasons} from '../mysql/mysql'
 import {addReportReason} from '../mysql/mysql'
 import secSettings from '../res/security_settings.json';
+import {ErrorResponse} from '@socialstuff/utilities/responses';
+import {body} from 'express-validator';
+
 const reportCreationInterface = Router();
+
 
 async function getAllReports(req:Request, res: Response) {
   console.log("report creation has been called");
@@ -10,12 +14,16 @@ async function getAllReports(req:Request, res: Response) {
   return res.json(reports).end();
 }
 
+//TODO return correct error message
 async function addAReportReason(req: Request, res: Response) {
+ // let responseBody:ErrorResponse = {errors: {reason: {msg: "Object is already in database"}} }
   //return connection.query('SELECT * FROM report');
   //console.log("Adding reason: ", req.body);
-  await addReportReason(req, res);
-  console.log("reason should be added");
-  res.status(200);
+  const responseCode = await addReportReason(req, res);
+  //responseBody
+  console.log("Status code: ", responseCode);
+  res.status(responseCode);
+  //res.json(responseBody);
   //return undefined;
 }
 
