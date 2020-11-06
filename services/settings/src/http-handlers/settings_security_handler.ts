595ce4c3bd605cacc0fdcc82337936c7f48ac9c2
tobias-jansen-2411 <B3ll3n!95>
2020-11-06 12:00:12 +0100

    removed unnecessary postgres_client.ts

--- services/settings/src/http-handlers/settings_security_handler.ts
+++ services/settings/src/http-handlers/settings_security_handler.ts
@@ -42,7 +42,7 @@ async function changeSecuritySettings(req: Request, res: Response) {
   await file.set('individual_pwd_req.reg_ex_string', body.individual_pwd_req.reg_ex_string);
   await file.set('inv_only.on', body.inv_only.on);
   await file.set('inv_only.inv_only_by_adm', body.inv_only.inv_only_by_adm);
-  file.save();
+  await file.save();
   await setTimeout(() => res.json(secSettings), 2000);
 
 
