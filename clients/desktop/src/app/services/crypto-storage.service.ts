0cc83c275637fcd2881e570bc5d6aed6afb4027d
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-11-18 14:40:17 +0100

    implemented crypto storage

--- clients/desktop/src/app/services/crypto-storage.service.ts
+++ clients/desktop/src/app/services/crypto-storage.service.ts
@@ -1,51 +1,70 @@
-import {app} from 'electron';
-import {CryptoProvider, CryptoStorage} from '@trale/persistence/crypto-storage';
-import {Injectable} from '@angular/core';
-import * as path from 'path';
-import {hashUnique} from "@socialstuff/utilities/security";
-import {BinaryLike, createCipheriv, createDecipheriv, createHash, randomBytes} from "crypto";
-import * as fs from 'fs';
+import {Injectable}                                                            from '@angular/core';
+import {CryptoProvider, CryptoStorage}                                         from '@trale/persistence/crypto-storage';
+import {BinaryLike, createCipheriv, createDecipheriv, createHash, randomBytes} from 'crypto';
+import * as fs                                                                 from 'fs';
+import * as os                                                                 from 'os';
+import * as path                                                               from 'path';
+import {Observable, Subject}                                                   from 'rxjs';
 
 function cryptoProviderFactory(key: Buffer): CryptoProvider {
   return {
     encrypt(data: BinaryLike) {
       const iv = randomBytes(16);
-      const cipher = createCipheriv('aes-256-cbc', key, Buffer.alloc(0));
+      const cipher = createCipheriv('aes-256-cbc', key, iv);
       return Buffer.concat([iv, cipher.update(data), cipher.final()]);
     },
     decrypt(data: Buffer) {
       const iv = data.slice(0, 16);
       const decipher = createDecipheriv('aes-256-cbc', key, iv);
       return Buffer.concat([decipher.update(data.slice(16)), decipher.final()]);
-    }
+    },
   };
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CryptoStorageService {
-
-  private _storage: CryptoStorage;
-  private _path = path.join(app.getPath('home'), '.trale');
-  private _masterKey: Buffer;
-
-  constructor() {
+  get path(): string {
+    return this._path;
   }
 
+  get masterKey(): Buffer {
+    return this._masterKey;
+  }
+
+  get isLoaded(): Observable<void> {
+    return this._isLoaded;
+  }
+
+
+  private _storage: CryptoStorage;
+  private _path: string;
+  private _masterKey: Buffer;
+  private _isLoaded = new Subject<void>();
+
   public async load(username: string, key: Buffer) {
+    this._path = path.join(os.homedir(), '.trale');
+    // .trale main directory
+    try {
+      await fs.promises.stat(this._path);
+    } catch (e) {
+      await fs.promises.mkdir(this._path);
+    }
     const usernameHash = (() => {
-      const sha = createHash('sha-512');
+      const sha = createHash('sha512');
       sha.update(username);
       return sha.digest().toString('hex');
     })();
+    // Trale user folder
     this._path = path.join(this._path, usernameHash);
-    {
-      const pathStat = await fs.promises.stat(this._path);
-      if (!pathStat.isDirectory()) {
-        await fs.promises.mkdir(this._path);
-      }
+    try {
+      await fs.promises.stat(this._path);
+      await fs.promises.stat(this._path);
+    } catch (e) {
+      await fs.promises.mkdir(this._path);
     }
+    // Master key for encrypted storage
     {
       const masterKeyPath = path.join(this._path, 'master.key');
       try {
@@ -61,8 +80,21 @@ export class CryptoStorageService {
         await fs.promises.writeFile(masterKeyPath, maskedKey);
       }
     }
+    const traleUserSpaceFolders = ['chats', 'media'];
+    // user space chat folder
+    {
+      for (const f of traleUserSpaceFolders) {
+        const p = path.join(this._path, f);
+        try {
+          await fs.promises.stat(p);
+        } catch (e) {
+          await fs.promises.mkdir(p);
+        }
+      }
+    }
 
     this._storage = new CryptoStorage(this._path, cryptoProviderFactory(this._masterKey));
+    this._isLoaded.next();
   }
 
   get storage(): CryptoStorage {
