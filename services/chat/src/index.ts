940c35c2d258bbecbc34a2aab65dce638e6cd2c5
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-11-23 17:32:19 +0100

    added titp server initialization

--- services/chat/src/index.ts
+++ services/chat/src/index.ts
@@ -18,12 +18,11 @@ import bootstrap                             from './bootstrap';
 import express                               from 'express';
 import {injectProcessEnvironmentIntoRequest} from '@socialstuff/utilities/express';
 import util                                  from 'util';
-
-
-import foo from './http-handlers/foo';
+import {server} from './titp-server';
 
 const APP_PORT = parseInt(process.env.APP_PORT || '3000');
-const APP_HOST = process.env.APP_HOST || '0.0.0.0';
+const APP_HOST = process.env.APP_HOST || '127.0.0.1';
+const TRALE_PORT = parseInt(process.env.TRALE_PORT || '3002');
 
 (async () => {
   await bootstrap;
@@ -38,15 +37,25 @@ const APP_HOST = process.env.APP_HOST || '0.0.0.0';
     next();
   });
 
-  app.post('/foo', foo);
+  // app.post('/foo', foo);
+  try {
+    await server.listen({ host: APP_HOST, port: TRALE_PORT });
+    console.log(`Trale Server running on host ${APP_HOST} and port ${TRALE_PORT}.`);
+  } catch {
+    console.error(`Could not bind Trale server to port ${TRALE_PORT}!`);
+    process.exit(1);
+  }
 
+  server.newConnection().subscribe(x => {
+    console.log('new connection from:', x.username());
+  });
 
   try {
     const appListen: (port: number, host: string) => Promise<void> = util.promisify(app.listen.bind(app));
     await appListen(APP_PORT, APP_HOST);
   } catch (err) {
     console.error(err);
-    return;
+    process.exit(2);
   }
-  console.log(`Social Stuff Chat service running on ${APP_HOST}:${APP_PORT}.`);
+  console.log(`Social Stuff Chat service running on host ${APP_HOST} and port ${APP_PORT}.`);
 })();
