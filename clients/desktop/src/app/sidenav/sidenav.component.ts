dad399b96a0b311fc1756f97f409c25d496164da
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-11-27 18:31:19 +0100

    updated sidenav logic

--- clients/desktop/src/app/sidenav/sidenav.component.ts
+++ clients/desktop/src/app/sidenav/sidenav.component.ts
@@ -1,29 +1,41 @@
-import { Component, OnInit }                   from '@angular/core';
-import {ChatMenuItem, createEmptyChatMenuItem} from "../models/ChatMenuItem";
-import {UtilService}                           from "../services/util.service";
-import {ContactService}                        from '../services/contact.service';
+import {Component, OnInit}         from '@angular/core';
+import {ChatMenuItem, fromContact} from '../models/ChatMenuItem';
+import {UtilService}               from '../services/util.service';
+import {ContactService}    from '../services/contact.service';
+import {searchMatch}       from '../../lib/helpers';
 
 @Component({
-  selector: 'app-sidenav',
+  selector:    'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls:   ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
   public chats: ChatMenuItem[];
   public loadingContacts = true;
+  public contactName: string = '';
 
   constructor(
     private utils: UtilService,
     private contacts: ContactService,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.chats = [];
 
     this.contacts.isLoaded.subscribe(() => {
-      this.chats = this.contacts.readContacts().map(x => ({ id: 0, username: x.username, realName: x.displayName, acronym: this.utils.generateAcronym(x.displayName || '') }));
+      this.chats = this.contacts.readContacts().map(fromContact);
       this.loadingContacts = false;
     });
+  }
+
+  searchContacts() {
+    if (this.contactName === '') {
+      return this.contacts.readContacts();
+    }
+    return this.contacts.readContacts()
+      .filter(x => searchMatch(this.contactName, x.username)
+                   || searchMatch(this.contactName, x.displayName || ''));
   }
 
 }
