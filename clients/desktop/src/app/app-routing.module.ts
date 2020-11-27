a1bba1dafe8309a6b8e5edb822ca30d617bec340
Malte Castner <castnermalte@googlemail.com>
2020-11-27 10:48:33 +0100

    Merged changes from master

--- clients/desktop/src/app/app-routing.module.ts
+++ clients/desktop/src/app/app-routing.module.ts
@@ -4,11 +4,19 @@ import {RegisterComponent}    from './auth/register/register.component';
 import {LoginComponent}       from './auth/login/login.component';
 import {ChatViewComponent}    from './chat-view/chat-view.component';
 import {ChatAppComponent}     from './chat-app/chat-app.component';
-import {ForgotPasswordComponent} from "./auth/forgot-password/forgot-password.component";
+import {ForgotPasswordComponent} from './auth/forgot-password/forgot-password.component';
+import {SettingsViewComponent} from './admin/settings-view/settings-view.component';
+import {SecurityComponent} from './admin/security/security.component';
+import {DashboardComponent} from './admin/dashboard/dashboard.component';
+import {CreateInviteComponent} from './admin/usersManagement/CreateInvites/create-invite/create-invite.component';
+import {UsersComponent} from './admin/usersManagement/users/users.component';
+import {ReportSettingsComponent} from './admin/usersManagement/ReportSystem/ReportSettings/report-settings/report-settings.component';
+import {ReportedUsersComponent} from './admin/usersManagement/ReportSystem/ReportedUsers/reported-users/reported-users.component';
+import {BlockedUsersComponent} from './admin/usersManagement/blocked-users/blocked-users.component';
 import {LogoutComponent} from "./auth/logout/logout.component";
 import {LandingComponent} from "./landing/landing.component";
 
-const routes: Routes = [
+export const routesChat: Routes = [
   {
     path:       '',
     redirectTo: 'register',
@@ -27,9 +35,89 @@ const routes: Routes = [
   // }
 ];
 
+export const routesSettings: Routes = [
+  {
+    path: '',
+    component: SettingsViewComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+        data: {
+          name: 'Dashboard',
+          icon: 'dashboard'
+        }
+      },
+      {
+        path: 'security',
+        component: SecurityComponent,
+        data: {
+          name: 'Security',
+          icon: 'https'
+        }
+      },
+      {
+        path: 'users',
+        redirectTo: 'users/create-invite',
+        data: {
+          ignore: true
+        }
+      },
+      {
+        path: 'users',
+        component: UsersComponent,
+        data: {
+          name: 'Users',
+          icon: 'people'
+        },
+        children: [
+          {
+            path: 'create-invite',
+            component: CreateInviteComponent,
+            data: {
+              name: 'Create Invite',
+              icon: 'add_to_que',
+              parent: 'users'
+            },
+          },
+          {
+            path: 'report-settings',
+            component: ReportSettingsComponent,
+            data: {
+              name: 'Report Settings',
+              icon: 'how_to_reg',
+              parent: 'users'
+            }
+          },
+          {
+            path: 'reported-users',
+            component: ReportedUsersComponent,
+            data: {
+              name: 'Reported Users',
+              icon: 'report',
+              parent: 'users'
+            }
+          },
+          {
+            path: 'blocked-users',
+            component: BlockedUsersComponent,
+            data: {
+              name: 'Blocked Users',
+              icon: 'block',
+              parent: 'users'
+            }
+          },
+        ]
+      },
+    ]
+  },
+];
+
+const routes = routesChat.concat(routesSettings);
+
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routesSettings),
   ],
   exports: [RouterModule],
 })
--- clients/desktop/src/app/app.module.ts
+++ clients/desktop/src/app/app.module.ts
@@ -32,12 +32,42 @@ import {ChatAppComponent}        from './chat-app/chat-app.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { LogoutComponent } from './auth/logout/logout.component';
 import { LandingComponent } from './landing/landing.component';
-import {ScrollingModule} from "@angular/cdk/scrolling";
+import {ScrollingModule} from '@angular/cdk/scrolling';
 import { MessageBoxComponent } from './message-box/message-box.component';
+
+import { SettingsViewComponent } from './admin/settings-view/settings-view.component';
+import { NavigationContainerComponent } from './admin/SideNav/navigation-container/navigation-container.component';
+import { NavigationItemComponent } from './admin/SideNav/navigation-item/navigation-item.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { SecurityComponent } from './admin/security/security.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { CreateInviteComponent } from './admin/usersManagement/CreateInvites/create-invite/create-invite.component';
+import { ReportSettingsComponent } from './admin/usersManagement/ReportSystem/ReportSettings/report-settings/report-settings.component';
+import { ReportedUsersComponent } from './admin/usersManagement/ReportSystem/ReportedUsers/reported-users/reported-users.component';
+import { BlockedUsersComponent } from './admin/usersManagement/blocked-users/blocked-users.component';
+import { UsersComponent } from './admin/usersManagement/users/users.component';
+import { HeadlineComponent } from './admin/core/headline/headline.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { CreateNewInviteComponent } from './admin/usersManagement/CreateInvites/create-new-invite/create-new-invite.component';
+import { InviteCodeListComponent } from './admin/usersManagement/CreateInvites/InviteCodeList/invite-code-list/invite-code-list.component';
+import { InviteCodeRowComponent } from './admin/usersManagement/CreateInvites/InviteCodeList/invite-code-row/invite-code-row.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {
+  NgxMatDatetimePickerModule, NgxMatNativeDateModule,
+  NgxMatTimepickerModule,
+  NgxNativeDateModule
+} from '@angular-material-components/datetime-picker';
+import { ReportReasonsComponent } from './admin/usersManagement/ReportSystem/ReportSettings/ReportReasons/report-reasons/report-reasons.component';
+import { ReportReasonEntryComponent } from './admin/usersManagement/ReportSystem/ReportSettings/ReportReasons/report-reason-entry/report-reason-entry.component';
+import { ReportListComponent } from './admin/usersManagement/ReportSystem/ReportedUsers/ReportList/report-list/report-list.component';
+import { ReportRowComponent } from './admin/usersManagement/ReportSystem/ReportedUsers/ReportList/report-row/report-row.component';
+import { ReasonsForReportComponent } from './admin/usersManagement/ReportSystem/ReportedUsers/ReportList/reasons-for-report/reasons-for-report.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
@@ -45,7 +75,42 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 }
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent, ChatListComponent, HeaderComponent, FooterComponent, ChatBubbleComponent, ChatViewComponent, ChatAppComponent, SidenavComponent, ForgotPasswordComponent, ContactInfoComponent, LogoutComponent, LandingComponent, MessageBoxComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChatListComponent,
+    HeaderComponent,
+    FooterComponent,
+    ChatBubbleComponent,
+    ChatViewComponent,
+    ChatAppComponent,
+    SidenavComponent,
+    ForgotPasswordComponent,
+    ContactInfoComponent,
+    LogoutComponent,
+    LandingComponent,
+    MessageBoxComponent,
+    SettingsViewComponent,
+    NavigationContainerComponent,
+    NavigationItemComponent,
+    SecurityComponent,
+    DashboardComponent,
+    CreateInviteComponent,
+    ReportSettingsComponent,
+    ReportedUsersComponent,
+    BlockedUsersComponent,
+    UsersComponent,
+    HeadlineComponent,
+    CreateNewInviteComponent,
+    InviteCodeListComponent,
+    InviteCodeRowComponent,
+    ReportReasonsComponent,
+    ReportReasonEntryComponent,
+    ReportListComponent,
+    ReportRowComponent,
+    ReasonsForReportComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -67,8 +132,16 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     BrowserAnimationsModule,
     MatInputModule,
     MatDialogModule,
+    MatGridListModule,
+    MatSlideToggleModule,
     MatCheckboxModule,
     ScrollingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgxMatDatetimePickerModule,
+    NgxMatTimepickerModule,
+    NgxNativeDateModule,
+    NgxMatNativeDateModule
   ],
   providers:    [],
   bootstrap:    [AppComponent],
