2a21f7142aa06aa98dc9dc12cac051703c5bd4fc
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-11-27 10:14:31 +0100

    added contact search function

--- clients/desktop/src/lib/helpers.ts
+++ clients/desktop/src/lib/helpers.ts
@@ -7,3 +7,28 @@ export function hashUsername(username: string) {
     .digest()
     .toString('hex');
 }
+
+/**
+ * Determines whether the needle sequence is contained inside the haystack.
+ * The needle sequence does not have to be contained as a consecutive sequence!
+ * @param needle
+ * @param haystack
+ */
+export function searchMatch(needle: string, haystack: string) {
+  if (needle.length > haystack.length) {
+    return false;
+  } else if (needle.length === haystack.length) {
+    return needle === haystack;
+  } else {
+    let needleIndex = 0;
+    for (const c of haystack) {
+      if (c === needle[needleIndex]) {
+        ++needleIndex;
+      }
+      if (needleIndex === needle.length) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
