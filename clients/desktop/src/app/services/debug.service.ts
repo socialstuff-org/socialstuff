10386b0673bbde3c1a27d414300ed65216d6f888
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-11-27 18:33:40 +0100

    did debugging

--- clients/desktop/src/app/services/debug.service.ts
+++ clients/desktop/src/app/services/debug.service.ts
@@ -6,6 +6,7 @@ import * as os                from 'os';
 import {CryptoStorageService} from './crypto-storage.service';
 import {TitpServiceService}   from './titp-service.service';
 import {ChatMessageType}      from '@trale/transport/message';
+import {ContactService}       from './contact.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,7 @@ export class DebugService {
   constructor(
     private storage: CryptoStorageService,
     private titp: TitpServiceService,
+    private contacts: ContactService,
   ) { }
 
   public async loadSession() {
@@ -33,15 +35,20 @@ export class DebugService {
     const result = { username: session.username, key: Buffer.from(session.key.data) };
     await this.storage.load(result.username, result.key);
 
+    console.log('connecting to chat service...');
     await this.titp.connect(session.username, '127.0.0.1', 3002);
-    this.titp
-      .client.sendChatMessageTo({
-      content: Buffer.from('Hello, World!', 'utf-8'),
-      type: ChatMessageType.text,
-      senderName: result.username,
-      attachments: [],
-      sentAt: new Date()
-    }, ['foobar2'])
+    // TODO - still throws due to missing conversation key
+    // this.titp
+    //   .client.sendChatMessageTo({
+    //   content: Buffer.from('Hello, World!', 'utf-8'),
+    //   type: ChatMessageType.text,
+    //   senderName: result.username,
+    //   attachments: [],
+    //   sentAt: new Date()
+    // }, ['alice@127.0.0.1:8086']);
+    // this.titp.client.negotiateKeyWith('charlie@127.0.0.1:8086');
+    // const alice = await this.contacts.load('alice@127.0.0.1:8086');
+    // console.log(alice);
 
     return result;
   }
@@ -54,6 +61,7 @@ export class DebugService {
   }
 
   public async destroySession() {
+    await this.contacts.unLoad();
     await fs.promises.unlink(path.join(this.basePath, '.debug_session'));
   }
 }
