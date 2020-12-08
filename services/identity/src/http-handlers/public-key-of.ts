498f10deafecd9c51c2ad50c0041dc9eec182ad0
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-12-08 16:00:35 +0100

    added public key loading from foreign server

--- services/identity/src/http-handlers/public-key-of.ts
+++ services/identity/src/http-handlers/public-key-of.ts
@@ -5,30 +5,40 @@ import {rejectOnValidationError}             from '@socialstuff/utilities/expres
 import {injectDatabaseConnectionIntoRequest} from '../utilities';
 import {RequestWithDependencies}             from '../request-with-dependencies';
 import {DataResponse}                        from '@socialstuff/utilities/responses';
+import axios                                 from 'axios';
 
 const middleware = [
-  param('username').isString(),
-  rejectOnValidationError,
-  injectDatabaseConnectionIntoRequest
-]
+        param('username').isString(),
+        rejectOnValidationError,
+        injectDatabaseConnectionIntoRequest,
+      ]
 ;
 
 const selectPublicKeyQuery = 'SELECT public_key FROM users WHERE username LIKE ?;';
 
 export async function publicKeyOf(req: Request, res: Response) {
-  const db    = (req as RequestWithDependencies).dbHandle;
-  const [row] = await db.query<RowDataPacket[]>(selectPublicKeyQuery, req.params.username);
-  if (row.length === 0) {
-    res.status(404).end();
-    return;
-  }
-  const [{public_key}] = row;
-  const response: DataResponse<{public_key: string}> = {
-    data: {
-      public_key
+  const hostName = (req as RequestWithDependencies).env.APP_HOSTNAME || 'localhost';
+
+  const [username, server] = req.params.username.split('@');
+  if (server === undefined || server === hostName) {
+    const response: DataResponse<{ public_key: string }> = {
+      data: {
+        public_key: '',
+      },
+    };
+    const db = (req as RequestWithDependencies).dbHandle;
+    const [row] = await db.query<RowDataPacket[]>(selectPublicKeyQuery, username);
+    if (row.length === 0) {
+      res.status(404).end();
+      return;
     }
-  };
-  res.json(response);
+    const [{public_key}] = row;
+    response.data.public_key = public_key;
+    res.json(response);
+  } else {
+    const response = await axios.get<DataResponse<{ public_key: string }>>(`//${server}:8086/identity/public-key-of/${username}`);
+    res.json(response.data);
+  }
 }
 
 export default [middleware, publicKeyOf as any];
