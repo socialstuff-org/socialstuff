0c24f02d0d4f8be28e7f3bf38931fb2d898f89c9
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-12-09 13:08:59 +0100

    simplified members

--- clients/desktop/src/app/services/api.service.ts
+++ clients/desktop/src/app/services/api.service.ts
@@ -16,9 +16,9 @@ export class ApiService {
   get hostname(): string {
     return this._hostname;
   }
-  private endpoint = 'http://trale.org:8086';
-  private _hostname: string = 'trale.org';
+  private _hostname: string = 'joern-neumeyer.de';
   private _port: number = 8086;
+  private endpoint = `http://${this._hostname}:${this._port}`;
   private _tralePort: number = 8087;
 
   constructor() { }
