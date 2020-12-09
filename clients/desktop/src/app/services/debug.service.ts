0fe9b6be57a11e86976890587f967bedca9b37c2
MauritsvanderZee <maurits@van-der-zee.de>
2020-12-09 17:43:17 +0100

    added loading animation; adjusted text;

--- clients/desktop/src/app/services/debug.service.ts
+++ clients/desktop/src/app/services/debug.service.ts
@@ -51,7 +51,22 @@ export class DebugService {
     this.titp.client.onDisconnect().subscribe(async hadError => {
       Swal.fire({
         title: 'Disconnected!',
-        text: 'Please wait until we can reconnect you.',
+        html: 'Please wait until we can connect you...' +
+              '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; display: block; shape-rendering: auto;" width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">\n' +
+              '        <g transform="rotate(0 50 50)">\n' +
+              '          <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
+              '            <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>\n' +
+              '          </rect>\n' +
+              '        </g><g transform="rotate(120 50 50)">\n' +
+              '        <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
+              '          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>\n' +
+              '        </rect>\n' +
+              '      </g><g transform="rotate(240 50 50)">\n' +
+              '        <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
+              '          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>\n' +
+              '        </rect>\n' +
+              '      </g>\n' +
+              '      </svg>',
         allowOutsideClick: false,
         showCloseButton: false,
         showConfirmButton: false,
