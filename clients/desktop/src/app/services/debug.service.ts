a7a8975f314645fc0f869f4133c3aa143652a102
mauritsvanderzee <maurits@van-der-zee.de>
2020-12-09 21:18:43 +0100

    refactored swal; added time since reconnecting;

--- clients/desktop/src/app/services/debug.service.ts
+++ clients/desktop/src/app/services/debug.service.ts
@@ -1,16 +1,16 @@
-import {Injectable}           from '@angular/core';
-import {AppConfig}            from '../../environments/environment';
-import * as fs                from 'fs';
-import * as path              from 'path';
-import * as os                from 'os';
+import {Injectable} from '@angular/core';
+import {AppConfig} from '../../environments/environment';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
 import {CryptoStorageService} from './crypto-storage.service';
-import {TitpServiceService}   from './titp-service.service';
-import {ContactService}       from './contact.service';
-import {ApiService}           from './api.service';
-import {Router}               from '@angular/router';
-import Swal                   from 'sweetalert2';
-import {delay}                from '@socialstuff/utilities/common';
-import {interval}             from 'rxjs';
+import {TitpServiceService} from './titp-service.service';
+import {ContactService} from './contact.service';
+import {ApiService} from './api.service';
+import {Router} from '@angular/router';
+import Swal from 'sweetalert2';
+import {delay} from '@socialstuff/utilities/common';
+import {interval} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -28,25 +28,29 @@ export class DebugService {
   }
 
   private connectWithAnimation(session: any) {
+    let now = new Date().toLocaleTimeString();
+    const html = 'Please wait until we can connect you...<br>Reconnecting since: ' + now + '<br>\n' +
+      '\n' +
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; display: block; shape-rendering: auto;" width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">\n' +
+      '  <g transform="rotate(0 50 50)">\n' +
+      '    <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
+      '      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>\n' +
+      '    </rect>\n' +
+      '  </g><g transform="rotate(120 50 50)">\n' +
+      '  <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
+      '    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>\n' +
+      '  </rect>\n' +
+      '</g><g transform="rotate(240 50 50)">\n' +
+      '  <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
+      '    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>\n' +
+      '  </rect>\n' +
+      '</g>\n' +
+      '</svg>\n';
+
     return new Promise(async (res) => {
       Swal.fire({
         title: 'Disconnected!',
-        html: 'Please wait until we can connect you...' +
-              '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; display: block; shape-rendering: auto;" width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">\n' +
-              '        <g transform="rotate(0 50 50)">\n' +
-              '          <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
-              '            <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>\n' +
-              '          </rect>\n' +
-              '        </g><g transform="rotate(120 50 50)">\n' +
-              '        <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
-              '          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>\n' +
-              '        </rect>\n' +
-              '      </g><g transform="rotate(240 50 50)">\n' +
-              '        <rect x="0" y="30.5" rx="0" ry="0" width="100" height="3" fill="#ee4540">\n' +
-              '          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>\n' +
-              '        </rect>\n' +
-              '      </g>\n' +
-              '      </svg>',
+        html: html,
         allowOutsideClick: false,
         showCloseButton: false,
         showConfirmButton: false,
