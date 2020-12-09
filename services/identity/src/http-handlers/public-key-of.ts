0b8b25f3e05a6b2918ebe5cd6e47122bff8e9fd7
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-12-09 16:23:17 +0100

    added error handling, if the remote key cannot be loaded

--- services/identity/src/http-handlers/public-key-of.ts
+++ services/identity/src/http-handlers/public-key-of.ts
@@ -3,9 +3,9 @@ import {param}                               from 'express-validator';
 import {RowDataPacket}                       from 'mysql2/promise';
 import {rejectOnValidationError}             from '@socialstuff/utilities/express';
 import {injectDatabaseConnectionIntoRequest} from '../utilities';
-import {RequestWithDependencies}             from '../request-with-dependencies';
-import {DataResponse}                        from '@socialstuff/utilities/responses';
-import axios                                 from 'axios';
+import {RequestWithDependencies}     from '../request-with-dependencies';
+import {DataResponse, ErrorResponse} from '@socialstuff/utilities/responses';
+import axios                         from 'axios';
 
 const middleware = [
         param('username').isString(),
@@ -36,8 +36,13 @@ export async function publicKeyOf(req: Request, res: Response) {
     response.data.public_key = public_key;
     res.json(response);
   } else {
-    const response = await axios.get<DataResponse<{ public_key: string }>>(`//${server}:8086/identity/public-key-of/${username}`);
-    res.json(response.data);
+    try {
+      const response = await axios.get<DataResponse<{ public_key: string }>>(`//${server}:8086/identity/public-key-of/${username}`);
+      res.json(response.data);
+    } catch {
+      const r: ErrorResponse = { errors: { _http: { location: server, value: req.params.username, param: 'username', msg: `Could not load public key for user ${req.params.username}!` } } };
+      res.status(500).json(r).end();
+    }
   }
 }
 
