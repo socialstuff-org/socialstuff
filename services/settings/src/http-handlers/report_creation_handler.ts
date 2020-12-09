a28da0e8ec80bc8cc0ceffcc1ed5af440ed6100e
tobias_jansen_2411 <tobiasjansen2411@gmail.com>
2020-12-09 11:22:35 +0100

    added response val

--- services/settings/src/http-handlers/report_creation_handler.ts
+++ services/settings/src/http-handlers/report_creation_handler.ts
@@ -1,27 +1,32 @@
-import {Request, Response, Router} from 'express'
-import {getReportReasons} from '../mysql/mysql'
+import {json, Request, Response, Router} from 'express';
+import {getReportReasons, updateReasonRequest} from '../mysql/mysql';
 import {insertReportReason} from '../mysql/mysql'
 import secSettings from '../res/security_settings.json';
 import {ErrorResponse} from '@socialstuff/utilities/responses';
-import {body} from 'express-validator';
+import {body, ValidationChain} from 'express-validator';
 
 const reportCreationInterface = Router();
-
 
 async function getAllReports(req:Request, res: Response) {
   console.log("report creation has been called");
   const reports = await getReportReasons();
-  res.json(reports).end();
-
+  res.json(reports);
 }
+
 //TODO return correct error message
 async function addAReportReason(req: Request, res: Response) {
-  const responseCode = await insertReportReason(req, res);
-  console.log("Status code addAReportReason: ", responseCode);
-  res.status(responseCode);
+  const responseBody = await insertReportReason(req.body);
+  console.log("Status code addAReportReason: ", responseBody);
+  res.status(200).json(responseBody);
+}
+
+async function updateReportReason(req: Request, res: Response) {
+  const responseBody = await updateReasonRequest(req.body);
+  res.status(200),json(responseBody);
 }
 
 reportCreationInterface.post("/", addAReportReason);
+reportCreationInterface.put('/', updateReportReason);
 reportCreationInterface.get("/", getAllReports);
 reportCreationInterface.delete("/");
 
