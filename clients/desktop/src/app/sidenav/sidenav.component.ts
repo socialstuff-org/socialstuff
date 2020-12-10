0961bb66f0dc0240aae549307368ce691871fc7d
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-12-10 23:06:49 +0100

    fixed side nav contact loading

--- clients/desktop/src/app/sidenav/sidenav.component.ts
+++ clients/desktop/src/app/sidenav/sidenav.component.ts
@@ -1,45 +1,64 @@
-import {Component, OnInit}          from '@angular/core';
-import {UtilService}                from '../services/util.service';
-import {ContactService}             from '../services/contact.service';
-import {acronymOfName, searchMatch} from '../../lib/helpers';
-import {TitpServiceService}         from '../services/titp-service.service';
-import { prefix }                   from '@trale/transport/log';
-import {ContactWithLastMessage}     from '../models/Contact';
+import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../services/util.service';
+import { ContactService } from '../services/contact.service';
+import { acronymOfName, searchMatch } from '../../lib/helpers';
+import { TitpServiceService } from '../services/titp-service.service';
+import { prefix } from '@trale/transport/log';
+import { ContactWithLastMessage } from '../models/Contact';
+import { ChatMessage } from '@trale/transport/message';
+import { KeyRegistryService } from 'app/services/key-registry.service';
+import { ApiService } from 'app/services/api.service';
 
 const log = prefix('clients/desktop/app/sidenav-component');
 
 @Component({
-  selector:    'app-sidenav',
+  selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls:   ['./sidenav.component.scss'],
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-  public chats: ContactWithLastMessage[];
+  public chats: ContactWithLastMessage[] = [];
   public loadingContacts = true;
   public searchTerm = '';
 
   constructor(
-    private utils: UtilService,
     public contacts: ContactService,
-    private titp: TitpServiceService
+    private titp: TitpServiceService,
+    private api: ApiService,
   ) {
   }
 
   ngOnInit() {
-    this.chats = [];
     this.contacts.onContactListUpdated().subscribe(async () => {
       this.chats = await this.contacts.loadLastMessages(this.contacts.readContacts() as any);
-      this.loadingContacts = false;
-      log('got the following chats:', this.chats);
+      log('got the following chats with last messages:', this.chats);
     });
+
+    let sub;
 
     this.titp.onConnectionStateChanged.subscribe(isConnected => {
       if (isConnected) {
-        this.titp.client.incomingMessage().subscribe(message => {
-          console.log('incoming message:', message);
-        });
+        sub = this.titp.client.incomingMessage().subscribe(this._handleIncomingMessage.bind(this));
+      } else {
+        sub.unsubscribe();
+      }
+      if (isConnected && this.loadingContacts) {
+        this.loadingContacts = false;
+        this.contacts.load(this.titp.client.username() + '@' + this.api.hostname);
       }
     });
+  }
+
+  private async _handleIncomingMessage(message: ChatMessage) {
+    // if (message.type !== ChatMessageType.handshakeInitialization) {
+    //   return;
+    // }
+    // const c: Contact = {
+    //   username: message.senderName,
+    //   displayName: '',
+    //   rsaPublicKey: await this.keys.fetchRsa(message.senderName),
+    //   conversationKey
+    // };
   }
 
   public acronym(name: string) {
@@ -52,7 +71,7 @@ export class SidenavComponent implements OnInit {
     }
     return this.contacts.readContacts()
       .filter(x => searchMatch(this.searchTerm, x.username)
-                   || searchMatch(this.searchTerm, x.displayName || ''));
+        || searchMatch(this.searchTerm, x.displayName || ''));
   }
 
 }
