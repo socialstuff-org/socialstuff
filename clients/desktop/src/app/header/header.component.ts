3381fe97b2f173c891aa7824a41ba2b398de7f0c
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-12-10 16:19:56 +0100

    fixed version overwrite

--- clients/desktop/src/app/header/header.component.ts
+++ clients/desktop/src/app/header/header.component.ts
@@ -1,10 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {ChatPartner} from "../models/ChatPartner";
-import {MatDialog} from "@angular/material/dialog";
-import {ContactInfoComponent} from "../contact-info/contact-info.component";
-import {TitpServiceService} from '../services/titp-service.service';
-import Swal from "sweetalert2";
+import {Component, Input, OnInit}     from '@angular/core';
+import {Router}                                             from '@angular/router';
+import {ChatPartner}                                        from '../models/ChatPartner';
+import {MatDialog}                                          from '@angular/material/dialog';
+import {ContactInfoComponent}                               from '../contact-info/contact-info.component';
+import {TitpServiceService}                                 from '../services/titp-service.service';
+import Swal                                                 from 'sweetalert2';
+import {ContactService}                                     from '../services/contact.service';
+import {Contact}                                            from '../models/Contact';
+import {hashUsername, hashUsernameHmac}                     from '../../lib/helpers';
+import {KeyRegistryService}                                 from '../services/key-registry.service';
+import {ChatMessage, ChatMessageType, serializeChatMessage} from '@trale/transport/message';
+import {CryptoStorageService}                               from '../services/crypto-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -20,6 +26,9 @@ export class HeaderComponent implements OnInit {
     private router: Router,
     public dialog: MatDialog,
     public titp: TitpServiceService,
+    private contacts: ContactService,
+    private keys: KeyRegistryService,
+    private storage: CryptoStorageService
   ) {
   }
 
@@ -47,8 +56,8 @@ export class HeaderComponent implements OnInit {
     });
   }
 
-  public addNewContact() {
-    Swal.fire({
+  public async addNewContact() {
+    const swalResult = await Swal.fire({
       title: 'Add contact',
       text: 'Enter the username you want to connect with (e.g. sampleuser@myserver.com)',
       input: 'text',
@@ -58,33 +67,41 @@ export class HeaderComponent implements OnInit {
       showCancelButton: true,
       confirmButtonText: 'Connect',
       showLoaderOnConfirm: true,
-      preConfirm: (login) => {
-        // TODO initiate handshake
-        // TODO create chat partner directory with chat properties
-        // TODO create "chat message" with content "Handshake initiated, please wait until the handshake is finalized!"
-        return fetch(`//api.github.com/users/${login}`)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error(response.statusText)
-            }
-            return response.json()
-          })
-          .catch(error => {
-            Swal.showValidationMessage(
-              `Request failed: ${error}`
-            )
-          })
+      preConfirm: async (username) => {
+        try {
+          await this.titp.client.negotiateKeyWith(username);
+        } catch {
+          Swal.showValidationMessage('Could not start key negotiation. Perhaps the username is invalid.');
+          return;
+        }
+        const contact: Contact = {
+          username,
+          usernameHash: hashUsernameHmac(username, this.storage.masterKey),
+          rsaPublicKey: await this.keys.fetchRsa(username),
+          conversationKey: Buffer.alloc(0)
+        };
+        await this.contacts.addContact(contact);
+        const chat = await this.contacts.openChat(contact);
+        const initialMessage: ChatMessage = {
+          senderName: this.titp.client.username(),
+          content: Buffer.alloc(0),
+          type: ChatMessageType.handshakeInitialization,
+          sentAt: new Date(),
+          attachments: []
+        };
+        await chat.addRecord(serializeChatMessage(initialMessage));
+        await chat.close();
+        return contact;
       },
       allowOutsideClick: () => !Swal.isLoading()
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          icon: 'success',
-          title: 'Success',
-          text: 'Your handshake to $username has been initiated. Please wait for handshake to be finalized!',
-        })
-      }
-    })
+    });
+    if (swalResult.isConfirmed) {
+      await Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: `Your handshake with ${swalResult.value.username} has been initiated. Please wait for handshake to be finalized!`,
+      });
+    }
   }
 
 
