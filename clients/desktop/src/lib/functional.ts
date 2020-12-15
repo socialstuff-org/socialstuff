9618ffd2fb0bd4ca84fb1d050ccb31a25bc3970e
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-12-15 16:39:05 +0100

    fixed 'take' function

--- clients/desktop/src/lib/functional.ts
+++ clients/desktop/src/lib/functional.ts
@@ -1,5 +1,13 @@
 
-export function take<T>(generator: Generator<T> | AsyncGenerator<T>, amount?: number) {
+function extractValuesFromIterator<T>(i: { value: T, done: boolean }[]): T[] {
+  return i.filter(x => !x.done).map(x => x.value);
+}
+
+export function take<T>(generator: Generator<T>): ((n: number) => T[]);
+export function take<T>(generator: Generator<T>, amount: number): T[];
+export function take<T>(generator: AsyncGenerator<T>): ((n: number) => Promise<T[]>);
+export function take<T>(generator: AsyncGenerator<T>, amount: number): Promise<T[]>;
+export function take<T>(generator: Generator<T> | AsyncGenerator<T>, amount?: number): ((n: number) => T[] | Promise<T[]>) | T[] | Promise<T[]> {
   if (!amount) {
     return (n: number) => {
       if (n < 1) {
@@ -7,20 +15,19 @@ export function take<T>(generator: Generator<T> | AsyncGenerator<T>, amount?: nu
       }
       n = Math.floor(n);
       const result = [];
-      // @ts-ignore
-      for (const g of generator) {
-        result.push(g);
-        if (--n === 0) {
-          break;
-        }
-      }
+      do {
+        result.push(generator.next());
+      } while (--n > 0);
+      
+
       if (result[0] instanceof Promise) {
-        return Promise.all(result);
+        return Promise.all(result).then(extractValuesFromIterator) as Promise<T[]>;
       } else {
-        return result;
+        return extractValuesFromIterator(result);
       }
     };
   } else {
+    // @ts-ignore
     return take(generator)(amount);
   }
 }
