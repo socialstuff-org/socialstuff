b94041a8d4cefb86a4c392c7cc9df897a15d52d4
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-12-15 15:07:34 +0100

    adjusted logging

--- clients/desktop/src/app/services/debug.service.ts
+++ clients/desktop/src/app/services/debug.service.ts
@@ -93,17 +93,16 @@ export class DebugService {
     session.port && (this.api.port = session.port);
     session.tralePort && (this.api.tralePort = session.tralePort);
     console.log('connecting to chat service...');
-    const _connectPromise = this.connectWithAnimation(session);
     this.titp.onConnectionStateChanged.subscribe(isConnected => {
       if (isConnected) {
-        console.log('did connect!');
+        log('connected; listening for disconnects.');
         const _a = this.titp.client.onDisconnect().subscribe(hadError => {
           this.connectWithAnimation(session);
           _a.unsubscribe();
         });
       }
     });
-    await _connectPromise;
+    await this.connectWithAnimation(session);
     log('done with session loading');
     return result;
   }
