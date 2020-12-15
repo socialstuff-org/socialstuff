dfff2d33826d3ee120b3b537d29e85c1c86e5fe8
MauritsvanderZee <maurits@van-der-zee.de>
2020-12-15 14:24:31 +0100

    added frame false

--- clients/desktop/main.ts
+++ clients/desktop/main.ts
@@ -22,6 +22,7 @@ function createWindow(): BrowserWindow {
       nodeIntegration:             true,
       allowRunningInsecureContent: (serve) ? true : false,
     },
+    frame: false,
   });
 
   if (serve) {
