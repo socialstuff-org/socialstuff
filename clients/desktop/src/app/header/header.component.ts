7b06e4bebdf4df435c9c0bf254418f123b8704d6
MauritsvanderZee <maurits@van-der-zee.de>
2020-12-15 22:45:40 +0100

    fixed data loading in contact info panel

--- clients/desktop/src/app/header/header.component.ts
+++ clients/desktop/src/app/header/header.component.ts
@@ -1,25 +1,24 @@
-import {Component, Input, OnInit}     from '@angular/core';
+import {Component, Input, OnInit}                           from '@angular/core';
 import {Router}                                             from '@angular/router';
-import {ChatPartner}                                        from '../models/ChatPartner';
 import {MatDialog}                                          from '@angular/material/dialog';
-import {ContactInfoComponent} from '../contact-info/contact-info.component';
-import {TitpService}          from '../services/titp.service';
-import Swal                   from 'sweetalert2';
+import {ContactInfoComponent}                               from '../contact-info/contact-info.component';
+import {TitpService}                                        from '../services/titp.service';
+import Swal                                                 from 'sweetalert2';
 import {ContactService}                                     from '../services/contact.service';
 import {Contact}                                            from '../models/Contact';
-import {hashUsername, hashUsernameHmac}                     from '../../lib/helpers';
+import {hashUsernameHmac}                                   from '../../lib/helpers';
 import {KeyRegistryService}                                 from '../services/key-registry.service';
 import {ChatMessage, ChatMessageType, serializeChatMessage} from '@trale/transport/message';
 import {CryptoStorageService}                               from '../services/crypto-storage.service';
-import { prefix } from '@trale/transport/log';
-import { ApiService } from 'app/services/api.service';
+import {prefix}                                             from '@trale/transport/log';
+import {ApiService}                                         from 'app/services/api.service';
 
 const log = prefix('clients/desktop/components/header-component');
 
 @Component({
-  selector: 'app-header',
+  selector:    'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls:   ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
@@ -53,7 +52,7 @@ export class HeaderComponent implements OnInit {
     }
     this.contactInfoIsOpen = true;
     const dialogRef = this.dialog.open(ContactInfoComponent, {
-      data: {partner: this.chatPartner}
+      data: {chatPartner: this.chatPartner}
     });
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
@@ -64,16 +63,16 @@ export class HeaderComponent implements OnInit {
 
   public async addNewContact() {
     const swalResult = await Swal.fire({
-      title: 'Add contact',
-      text: 'Enter the username you want to connect with (e.g. sampleuser@myserver.com)',
-      input: 'text',
-      inputAttributes: {
+      title:               'Add contact',
+      text:                'Enter the username you want to connect with (e.g. sampleuser@myserver.com)',
+      input:               'text',
+      inputAttributes:     {
         autocapitalize: 'off'
       },
-      showCancelButton: true,
-      confirmButtonText: 'Connect',
+      showCancelButton:    true,
+      confirmButtonText:   'Connect',
       showLoaderOnConfirm: true,
-      preConfirm: async (username: string) => {
+      preConfirm:          async (username: string) => {
         if (!username.includes('@')) {
           username += '@' + this.titp.host;
         }
@@ -85,8 +84,8 @@ export class HeaderComponent implements OnInit {
         }
         const contact: Contact = {
           username,
-          usernameHash: hashUsernameHmac(username, this.storage.masterKey),
-          rsaPublicKey: await this.keys.fetchRsa(username),
+          usernameHash:    hashUsernameHmac(username, this.storage.masterKey),
+          rsaPublicKey:    await this.keys.fetchRsa(username),
           conversationKey: Buffer.alloc(0)
         };
         log('adding contact', contact);
@@ -95,10 +94,10 @@ export class HeaderComponent implements OnInit {
         const chat = await this.contacts.openChat(contact);
         log('opened chat with contact', contact)
         const initialMessage: ChatMessage = {
-          senderName: this.titp.client.userHandle,
-          content: Buffer.alloc(0),
-          type: ChatMessageType.handshakeInitialization,
-          sentAt: new Date(),
+          senderName:  this.titp.client.userHandle,
+          content:     Buffer.alloc(0),
+          type:        ChatMessageType.handshakeInitialization,
+          sentAt:      new Date(),
           attachments: []
         };
         log('initial message', initialMessage);
@@ -109,13 +108,13 @@ export class HeaderComponent implements OnInit {
         log('done closing chat for contact', contact);
         return contact;
       },
-      allowOutsideClick: () => !Swal.isLoading()
+      allowOutsideClick:   () => !Swal.isLoading()
     });
     if (swalResult.isConfirmed) {
       await Swal.fire({
-        icon: 'success',
+        icon:  'success',
         title: 'Success',
-        text: `Your handshake with ${swalResult.value.username} has been initiated. Please wait for handshake to be finalized!`,
+        text:  `Your handshake with ${swalResult.value.username} has been initiated. Please wait for handshake to be finalized!`,
       });
     }
   }
