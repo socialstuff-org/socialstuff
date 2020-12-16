94788326530de6571a97032ec041eccd3fac89db
tobias_jansen_2411 <tobiasjansen2411@gmail.com>
2020-12-16 10:15:48 +0100

    added documentation for ts reporting service

--- services/reporting/src/http-handlers/report_reason_handler.ts
+++ services/reporting/src/http-handlers/report_reason_handler.ts
@@ -6,33 +6,26 @@ import {injectDatabaseConnectionIntoRequest} from '../utilities';
 import {rejectOnValidationError} from '@socialstuff/utilities/express';
 
 import {RowDataPacket} from 'mysql2/promise';
-import {sharedConnection} from '../mysql';
 
 const reportReasonHandler = Router();
-
+/**
+ * Validates the fields body by checking if
+ * - max report violations is an Integer
+ * - reason is of type String
+ * - id is an Integer and optional
+ */
 export const middleware:ValidationChain[] = [
   body('max_report_violations').isInt(),
-  body('reason').isString().custom(async reason => {
-    console.log('Checking if reason already exists: ');
-    const db = await sharedConnection();
-    const sql = 'SELECT count(*) >= 1 AS present FROM report_reason WHERE reason LIKE UPPER(?);';
-
-    const [[{present}]] = await db.query<RowDataPacket[]>(sql, [reason]);
-
-    console.log('Reason already exists?' , present);
-    if (present){
-      console.log('reason already exists, throwing error!');
-      await Promise.reject('Reason already exists!');
-    } else {
-      console.log('reason not present yet');
-      return;
-    }
-
-  })
+  body('reason').isString(),
+  body('id').optional().isInt()
 ];
 
 
-
+/**
+ * retrieves all report reasons from the database
+ * @param req request from the client. Does not require any special arguments
+ * @param res response that will be returned to client
+ */
 async function getAllReportReasons(req: Request, res: Response) {
   const sql = 'SELECT * FROM report_reason;';
   const db = (req as RequestWithDependencies).dbHandle;
@@ -41,37 +34,76 @@ async function getAllReportReasons(req: Request, res: Response) {
   return res.status(200).json(reports[0]);
 }
 
+/**
+ * Method edits a report reason
+ * @param req includes a body that contains the new report reason in this format:
+ * {
+ *   id: '123'
+ *   reason: 'some reason'
+ *   max_report_violations: 15
+ * }
+ * @param res is the response that will be returned upon completion of the request. It contains the newly added reason in its body
+ */
 async function editReportReason(req: Request, res: Response) {
   const maxReportViolations = req.body.max_report_violations;
   const reason = req.body.reason;
   const sql = 'UPDATE report_reason SET reason = ?, max_report_violations = ? WHERE id = ?;';
   const db = (req as RequestWithDependencies).dbHandle;
+  console.log('Checking if reason already exists: ');
+
   try {
+    const sqlVerify = 'SELECT count(*) >= 1 AS present, id as id FROM report_reason WHERE reason LIKE UPPER(?);';
+    console.log('executing query');
+    const [[{present}]] = await db.query<RowDataPacket[]>(sqlVerify, [req.body.reason, req.body.max_report_violations]);
+    console.log('reason present? ', present);
+    if (present) {
+      console.log('ending request');
+      res.status(409).json({error: 'The report reason already exists'});
+      return;
+    }
+
     console.log('adding reason:     ', req.body.reason);
     console.log('With ID:           ', req.body.id);
     console.log('With max_report_v: ', maxReportViolations);
     await db.query(sql, [reason, maxReportViolations, req.body.id]);
+    const retSQL = 'SELECT * FROM report_reason WHERE id = ?;';
+    res.status(200).json((await db.query(retSQL, [req.body.id]))[0]);
   } catch (e) {
     console.log(e);
     res.status(500).end();
   }
 
-  const retSQL = 'SELECT * FROM report_reason WHERE id = ?;';
-  res.status(200).json((await db.query(retSQL, [req.body.id]))[0]);
 
 }
 
-
+/**
+ * Deletes a report reason in the database
+ * @param req request from client, containing the id of the reason as a header attribute
+ * @param res response that will be sent  back to the client,
+ */
 async function removeReportReason(req: Request, res: Response) {
-  console.log('Removing reason');
-  const db = (req as RequestWithDependencies).dbHandle;
-  const deleteFromSQL = 'DELETE FROM report_reason WHERE id = ?';
-  await db.query(deleteFromSQL, [req.headers.id]);
-  res.status(200).json({
-    msg: 'Deleted report reqson with id ' + req.headers.id,
-  });
+  try{
+    console.log('Removing reason');
+    const db = (req as RequestWithDependencies).dbHandle;
+    const deleteFromSQL = 'DELETE FROM report_reason WHERE id = ?';
+    await db.query(deleteFromSQL, [req.headers.id]);
+    res.status(200).json({
+      msg: 'Deleted report reason with id ' + req.headers.id,
+    });
+  } catch (e) {
+    res.status(500).json({error: 'Couldn\'t delete entry with ID: ' + req.headers.id});
+  }
 }
 
+/**
+ * adds a report reason tho the database
+ * @param req request from the client. Contains the new reason as a JSON in the format:
+ * {
+ *   "reason": "some new reason",
+ *   "max_report_violations": 50,
+ * }
+ * @param res
+ */
 async function addReportReason(req: Request, res: Response) {
   const db = (req as RequestWithDependencies).dbHandle;
   const insertRR = 'INSERT INTO report_reason (reason, max_report_violations) VALUES (?, ?)';
@@ -92,6 +124,9 @@ async function addReportReason(req: Request, res: Response) {
   }
 }
 
+/**
+ * validates if the header ID is an integer for the delete request
+ */
 export const validateHeader: ValidationChain[] = [header('id').isInt()];
 
 reportReasonHandler.use(injectProcessEnvironmentIntoRequest , injectDatabaseConnectionIntoRequest);
--- services/reporting/src/http-handlers/reporting.ts
+++ services/reporting/src/http-handlers/reporting.ts
@@ -2,9 +2,23 @@ import {Request, Response, Router} from 'express';
 import {RequestWithDependencies} from '../request-with-dependencies';
 import {RowDataPacket} from 'mysql2/promise';
 import {injectDatabaseConnectionIntoRequest} from '../utilities';
+import {body, header, ValidationChain} from 'express-validator';
 
 const reportingHandler = Router();
 
+/**
+ * used for reporting a user. Verifies if the user sending out the report has already reported the user for the same reason in the past 15 minutes.
+ * In that case the report will not pass. Otherwise the report will be registered in the database
+ * @param req request from the client. Follow this layout:
+ *  Headers:
+ *   - user_token
+**  Body:
+ * {
+ *   "username": "userHashOfUserBeingReported",
+ *   "reason_id": 123
+ * }
+ * @param res
+ */
 async function reportUser(req: Request, res: Response) {
   const sql = 'SELECT count(*) >= 1 AS alreadyReportedFROM report WHERE reported_by = ? AND timestamp >= NOW() - INTERVAL 15 MINUTE;';
   try {
@@ -22,6 +36,11 @@ async function reportUser(req: Request, res: Response) {
     res.status(500).end();
   }
 }
+export const validateRequest: ValidationChain[] = [
+  header('user_token').isString(),
+  body('reason_id').isInt(),
+  body('username').isString()
+];
 
 //import {body, ValidationChain} from 'express-validator';
-reportingHandler.post('/', injectDatabaseConnectionIntoRequest, reportUser);
+reportingHandler.post('/', validateRequest,injectDatabaseConnectionIntoRequest, reportUser);
