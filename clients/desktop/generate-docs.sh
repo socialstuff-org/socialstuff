491d3835734259b281884d586f62585266d2c3e5
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-12-16 19:15:33 +0100

    added script to generate documentation

---
+++ clients/desktop/generate-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo '{' > tsconfig.compodoc.json
+echo '  "extends": "./tsconfig.json",' >> tsconfig.compodoc.json
+echo '  "files": [' >> tsconfig.compodoc.json
+for i in $(find src/ -name "*.ts")
+do
+   echo '    "'$i'",' >> tsconfig.compodoc.json
+done
+echo '  ],' >> tsconfig.compodoc.json
+echo '}' >> tsconfig.compodoc.json
+npx @compodoc/compodoc -p tsconfig.compodoc.json
+rm tsconfig.compodoc.json
